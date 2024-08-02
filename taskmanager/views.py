0d0dceb3cd61985aebaeecad8d737102cf18b481
Mauritz Marais <mauritz303@gmail.com>
2024-08-02 15:38:20 +0200

    added board detail and new board views

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+from .models import Board
+from .forms import BoardForm, ColumnForm
 # Create your views here.
+
+def index(request):
+    all_boards = Board.objects.all()
+    return render(request, 'index.html', context={'all_boards': all_boards})
+
+def board_detail_view(request, id):
+    board = get_object_or_404(Board, id=id)
+    all_boards = Board.objects.all()
+
+    return render(request, 'components/board_detail.html', context={'board': board, 'all_boards': all_boards})
+
+def new_board(request):
+    if request.method == 'POST':
+        form = BoardForm(request.POST)
+        if form.is_valid():
+            board = form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = BoardForm()
+    return render(request, 'components/board_form.html', {
+        'form': form,
+    })
+
+def new_column(request, id):
+    if request.method == 'POST':
+        form = ColumnForm(request.POST)
+        if form.is_valid():
+            column = form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = ColumnForm()
+        
+    return render(request, 'components/column_form.html', {
+        'form': form
+    })
