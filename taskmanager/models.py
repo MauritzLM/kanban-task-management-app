d1204b5a8e2653c830e4c7b648295abf7752cabb
Mauritz Marais <mauritz303@gmail.com>
2024-08-02 15:35:50 +0200

    created models for app

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+import uuid
+
+# board model
+class Board(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('board-detail', args=[str(self.id)])
+    
+# column model
+class Column(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    board = models.ForeignKey(Board, related_name='columns', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+# task model
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    column = models.ForeignKey(Column, related_name='tasks', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('task-detail', args=[str(self.id)])      
+
+# subtask model
+class SubTask(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=50)
+    is_completed = models.BooleanField(default=False)
+    task = models.ForeignKey(Task, related_name='subtasks', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('subtask-detail', args=[str(self.id)])
