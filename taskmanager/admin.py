dde247498760c13609526c051903008f29586f33
Mauritz Marais <mauritz303@gmail.com>
2024-08-02 15:37:33 +0200

    registered models

--- taskmanager/admin.py
+++ taskmanager/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Board, Task, Column, SubTask
 # Register your models here.
+
+admin.site.register(Board)
+admin.site.register(Task)
+admin.site.register(Column)
+admin.site.register(SubTask)
