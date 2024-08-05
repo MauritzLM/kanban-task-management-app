9b2e82e0850a9c63532192bb8a2727a16227c2a9
Mauritz Marais <mauritz303@gmail.com>
2024-08-05 15:56:32 +0200

    added edit-board

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("board/new-board", views.new_board, name="new-board"),
+    path("board/new-board", views.board_form, name="new-board"),
+    path("board/column", views.column_form, name="add-column"),
     path("board/<str:id>" , views.board_detail_view, name="board-detail"),
-    path("board/<str:id>/new-column", views.new_column, name="new-column")
-    
+    path("board/<str:id>/edit-board", views.edit_board, name="edit-board")
 ]
