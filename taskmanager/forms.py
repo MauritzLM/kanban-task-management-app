faaf0b84c3ef080ebd2be86cc9e8c6bc309ff55e
Mauritz Marais <mauritz303@gmail.com>
2024-08-05 15:57:38 +0200

    changed to modelforms

--- taskmanager/forms.py
+++ taskmanager/forms.py
@@ -1,16 +1,17 @@
-from django import forms
+from django.forms import ModelForm
 from django.forms import formset_factory
 
 from .models import Board, Task, Column, SubTask
 
-class BoardForm(forms.Form):
-    name = forms.CharField(label="Name", max_length=50)
+class BoardForm(ModelForm):
+    class Meta:
+        model = Board
+        fields = ['name']
+    
 
 
-class ColumnForm(forms.Form):
-    name = forms.CharField(label="Name", max_length=50)
-    board = forms.HiddenInput()
-
-# how to dynamically create number?*
-# formset to display inside new board form
-ColumnFormSet = formset_factory(ColumnForm)    
+class ColumnForm(ModelForm):
+    class Meta:
+        model = Column
+        fields = ['name']   
+    
