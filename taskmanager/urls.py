34d91ab1483c0ecc66ef574371e720536c9383a5
Mauritz Marais <mauritz303@gmail.com>
2024-08-07 15:53:33 +0200

    updated new-board functionality

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("board/new-board", views.board_form, name="new-board"),
-    path("board/column", views.column_form, name="add-column"),
+    path("board/column/<int:current_total_formsets>", views.column_form, name="add-column"),
     path("board/<str:id>" , views.board_detail_view, name="board-detail"),
     path("board/<str:id>/edit-board", views.edit_board, name="edit-board")
 ]
--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from asgiref.sync import sync_to_async
 
+from .helpers import build_new_formset
 from .models import Board, Column
-from .forms import BoardForm, ColumnForm
+from .forms import BoardForm, ColumnFormSet
 
 # index page
 def index(request):
@@ -21,22 +23,28 @@ def board_detail_view(request, id):
 def board_form(request):
     if request.method == 'POST':
         board_form = BoardForm(request.POST)
-        column_form = ColumnForm(request.POST)
-        if board_form.is_valid() and column_form.is_valid():
-            created_board = board_form.save()
-            # get id from saved board
-            # how to set?*
-            column_form.cleaned_data['board'] = created_board.id 
-            column_form.save() 
+        column_formset = ColumnFormSet(request.POST)
+
+        if board_form.is_valid() and column_formset.is_valid():
+            # save board and get object
+            created_board = board_form.save()  
+
+            # set column id
+            columns = column_formset.save(commit=False)
+            for col in columns:
+                col.board = created_board
+                col.save()
+
             return HttpResponseRedirect(reverse('index'))
-    else:
-        context = {
-            'title': 'new',
-            'board_form': BoardForm(),
-            'column_form': ColumnForm()
-        }
         
-    return render(request, 'components/board_form.html', context)
+        # validate column fields*
+
+    else:
+        board_form = BoardForm()
+        column_formset = ColumnFormSet(queryset=Column.objects.none())
+            
+    return render(request, 'components/board_form.html', context={'board_form': board_form, 'formset': column_formset, 'title': 'New'})
+
 
 # edit board
 def edit_board(request, id):
@@ -45,7 +53,7 @@ def edit_board(request, id):
 
     if request.method == 'POST':
         board_form = BoardForm(request.POST)
-        column_form = ColumnForm(request.POST)
+        column_form = ColumnFormSet(request.POST)
         if board_form.is_valid() and column_form.is_valid():
             board_form.save()
             column_form.save()
@@ -54,7 +62,7 @@ def edit_board(request, id):
         context = {
             'title': 'edit',
             'board_form': BoardForm(),
-            'column_form': ColumnForm(),
+            'formset': ColumnFormSet(),
             'board_to_edit': board_to_edit,
             'board_columns': board_columns
         }
@@ -62,5 +70,11 @@ def edit_board(request, id):
     return render(request, 'components/edit_board_form.html', context)
 
 # column form
-def column_form(request):
-    return render(request, 'components/column_form.html', context={'column_form': ColumnForm()})
+def column_form(request, current_total_formsets):
+    new_formset = build_new_formset(ColumnFormSet(), current_total_formsets)
+    context = {
+        'new_formset': new_formset,
+        'new_total_formsets': current_total_formsets + 1,
+    }
+    
+    return render(request, 'components/column_form.html', context)
