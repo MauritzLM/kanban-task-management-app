e2934c1472800b52ac273377f617540d486418aa
Mauritz Marais <mauritz303@gmail.com>
2024-08-08 14:12:38 +0200

    added board delete path

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("board/new-board", views.board_form, name="new-board"),
     path("board/column/<int:current_total_formsets>", views.column_form, name="add-column"),
     path("board/<str:id>" , views.board_detail_view, name="board-detail"),
-    path("board/<str:id>/edit-board", views.edit_board, name="edit-board")
+    path("board/<str:id>/edit-board", views.edit_board, name="edit-board"),
+    path("board/<str:id>/delete-board", views.delete_board, name="delete-board")
 ]
