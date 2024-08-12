c32c6dcba12f0e7f0fdd1d33e497fe6a8a0ad1b4
Mauritz Marais <mauritz303@gmail.com>
2024-08-12 16:18:37 +0200

    added task urls for view and edit

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("board/new-board", views.board_form, name="new-board"),
     path("board/column/<int:current_total_formsets>", views.column_form, name="add-column"),
+    path("board/subtask/<int:current_total_formsets>", views.subtask_form, name="add-subtask"),
     path("board/<str:id>" , views.board_detail_view, name="board-detail"),
     path("board/<str:id>/edit-board", views.edit_board, name="edit-board"),
-    path("board/<str:id>/delete-board", views.delete_board, name="delete-board")
+    path("board/<str:id>/delete-board", views.delete_board, name="delete-board"),
+    path("board/<str:id>/new-task", views.new_task, name="new-task"),
+    path("board/<str:id>/view-task/<str:t_id>", views.task_view, name="view-task"),
+    path("board/<str:id>/edit-task/<str:t_id>", views.edit_task, name="edit-task")
 ]
