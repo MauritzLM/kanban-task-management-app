0e996152d33a94e663529ac168f095422a8dc7a2
Mauritz Marais <mauritz303@gmail.com>
2024-08-12 16:18:00 +0200

    added task forms

--- taskmanager/forms.py
+++ taskmanager/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from django.forms import modelformset_factory
+from django.utils.translation import gettext_lazy as _
 
 from .models import Board, Task, Column, SubTask
 
@@ -7,7 +8,11 @@ class BoardForm(ModelForm):
     class Meta:
         model = Board
         fields = ['name']
-    
+
+class DeleteBoardForm(ModelForm):
+    class Meta:
+        model = Board
+        fields = []    
 
 
 class ColumnForm(ModelForm):
@@ -18,4 +23,26 @@ class ColumnForm(ModelForm):
 
 # create column formset
 ColumnFormSet = modelformset_factory(Column, exclude=['board', 'id'], extra=0, can_delete=True)
+
+# for create new and edit
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'column']
+        labels = {
+            'column': _('Status'),
+        }
+
+# for viewing task
+class TaskViewForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['column']
+        labels = {
+            'column': _('Status'),
+        }
+
+
+SubTaskFormSet = modelformset_factory(SubTask, exclude=['task', 'id', 'is_completed'], extra=0, can_delete=True)
+TaskViewFormSet = modelformset_factory(SubTask, exclude=['task', 'id'], extra=0)  
     
