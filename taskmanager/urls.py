45be37b14ac0562f5beec3a124ad2d3dcf6e45a3
Mauritz Marais <mauritz303@gmail.com>
2024-08-14 11:23:01 +0200

    added delete and edit task urls

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("board/<str:id>/edit-board", views.edit_board, name="edit-board"),
     path("board/<str:id>/delete-board", views.delete_board, name="delete-board"),
     path("board/<str:id>/new-task", views.new_task, name="new-task"),
+    path("board/<str:t_id>/delete-task", views.delete_task, name="delete-task"),
     path("board/<str:id>/view-task/<str:t_id>", views.task_view, name="view-task"),
     path("board/<str:id>/edit-task/<str:t_id>", views.edit_task, name="edit-task")
 ]
