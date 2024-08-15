00629c05abab07106bf0a34d4061b4d767aceae0
Mauritz Marais <mauritz303@gmail.com>
2024-08-15 13:48:02 +0200

    added user field to board

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 import uuid
 
@@ -7,6 +8,7 @@ import uuid
 class Board(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
@@ -56,7 +58,7 @@ class SubTask(models.Model):
     task = models.ForeignKey(Task, related_name='subtasks', on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.sub_name
     
     def get_absolute_url(self):
         return reverse('subtask-detail', args=[str(self.id)])
