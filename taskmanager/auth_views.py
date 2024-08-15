b58e021f9a4e325ea1fd351cd4b0c10500ca6c12
Mauritz Marais <mauritz303@gmail.com>
2024-08-15 13:47:35 +0200

    added login, signup and logout view functions

--- taskmanager/auth_views.py
+++ taskmanager/auth_views.py
@@ -5,32 +5,74 @@ from django.http import HttpRequest, HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic.base import TemplateView
 
-from .auth_forms import SignUpForm
+from .auth_forms import LoginForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import logout, login, authenticate
 
 # signup
 class SignupView(TemplateView):
+    template_name = 'accounts/signup.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, context={'form': UserCreationForm()})
+    
+    def post(self, request, *args, **kwargs):
+       
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            # save user
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            # log user in
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+
+            return HttpResponseRedirect(reverse('index'))
+
+        return render(request, self.template_name, context={'form': form})
+
+# login
+class LoginView(TemplateView):
     template_name = 'accounts/login.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, context={'form': SignUpForm()})
+        return render(request, self.template_name, context={'form': LoginForm()})
     
     def post(self, request, *args, **kwargs):
-        if request.method == "POST":
-            form = SignUpForm(request.POST)
 
-            if form.is_valid():
-                # save user
-                form.save()
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            # log user in
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
 
                 return HttpResponseRedirect(reverse('index'))
 
-            return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name, context={'form': form})        
 
-# login
 
 
 # logout
+class LogoutView(TemplateView):
+    template_name = 'accounts/logout.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+    
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            logout(request)
+
+            return HttpResponseRedirect(reverse('index'))
 
 
 
