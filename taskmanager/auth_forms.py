11d7f94f37a92787007f22fea679fe09d48caffe
Mauritz Marais <mauritz303@gmail.com>
2024-08-30 10:35:27 +0200

    removed signup form

--- taskmanager/auth_forms.py
+++ taskmanager/auth_forms.py
@@ -1,11 +1,5 @@
-from django.forms import ModelForm
-from django import forms
-from django.contrib.auth.models import User
 
-class SignUpForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password', 'email']
+from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=65)
